7d2b0e2f17a238067948f746fe909d0d04f6d940
S-Maciejewski <sebastian_maciejewski@yahoo.com>
2019-06-05 14:45:57 +0200

    Observations

--- KartaPacjenta/server/app.js
+++ KartaPacjenta/server/app.js
@@ -10,7 +10,17 @@ const patientIDs = [
     1656327,
     1656300,
     1656311,
-    1656316
+    1656316,
+    // z obserwacjami
+    1909448, // obserwacja 1909478
+    295670, // obserwacja 295694
+
+]
+
+const observationIDs = [
+    1909478, // pacjent 1909448
+    434055, // bez pacjenta
+    295694, // pacjent 295670
 ]
 
 class Patient {
@@ -28,22 +38,48 @@ class Patient {
     }
 }
 
+class Observation {
+    constructor(id, text, subjectID, issuedDate, value, unit) {
+        this.id = id;
+        this.text = text;
+        this.subjectID = subjectID;
+        this.issuedDate = issuedDate;
+        this.value = value;
+        this.unit = unit;
+    }
+}
+
 async function getPatientData(id) {
     // return rp('http://hapi.fhir.org/baseDstu3/Patient/' + id + '/_history/' + version);  // Pobieranie wybranej wersji (czy implementujemy?)
     return rp('http://hapi.fhir.org/baseDstu3/Patient/' + id);
+}
 
+async function getObservationData(id) {
+    return rp('http://hapi.fhir.org/baseDstu3/Observation/' + id);
 }
 
 async function getPatient(id) {
     await getPatientData(id).then(res => {
-        res = JSON.parse(res)
+        res = JSON.parse(res);
         patient = new Patient(id, res.meta.versionId, res.meta.lastUpdated, res.gender ? res.gender : '',
             res.birthDate ? res.birthDate : '', res.active ? res.active : '',
             res.name && res.name[0] ? res.name[0].given[0] : '', res.name && res.name[0] ? res.name[0].family : '',
             res.address && res.address[0] ? res.address[0].text : '', res.address && res.address[0] ? res.address[0].city : '');
-    })
+    });
     console.log(`Patient ${id} retrieved from server successfully`);
     return patient;
+}
+
+async function getObservation(id) {
+    await getObservationData(id).then(res => {
+        res = JSON.parse(res);
+        observation = new Observation(id, res.code && res.code.text ? res.code.text : '', 
+        res.subject && res.subject.reference ? res.subject.reference.replace('Patient/', '') : '',
+        res.issued, res.valueQuantity && res.valueQuantity.value ? res.valueQuantity.value : '',
+        res.valueQuantity && res.valueQuantity.unit ? res.valueQuantity.unit : '');
+    });
+    console.log(`Observation ${id} retrieved from server successfully`);
+    return observation;
 }
 
 app.use(express.json());
@@ -56,5 +92,13 @@ app.get('/getPatient', (req, res) => {
 });
 
 app.get('/getPatientIDs', (req, res) => res.json(patientIDs));
+
+app.get('/getObservation', (req, res) => {
+    getObservation(req.query.id).then(observation => {
+        res.json(observation);
+    });
+});
+
+app.get('/getObservationIDs', (req, res) => res.json(observationIDs));
 
 app.listen(port, () => console.log(`Node server listening on port ${port}`));
